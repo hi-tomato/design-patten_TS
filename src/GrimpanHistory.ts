e08830dff281c2c03995e3ae66ba528e5b50dd1f
이승준 <tmdwns092727@nate.com>
2026-05-31 20:28:47 +0900

    Docs: Prototype Pattern

--- src/GrimpanHistory.ts
+++ src/GrimpanHistory.ts
@@ -1,16 +1,37 @@
 import type Grimpan from "./AbstractGrimpan";
 import type { ChromeGrimpan } from "./ChormeGrimpan";
 
+interface Cloneable {
+  clone(): Cloneable;
+}
+
+class HistoryStack extends Array implements Cloneable {
+  clone() {
+    return this.slice() as HistoryStack;
+  }
+}
+
+
 export abstract class GrimpanHistory {
   grimpan: Grimpan; 
+  stack: HistoryStack;
 
-  protected constructor(grimpan: Grimpan) {
+  protected constructor(grimpan: Grimpan, stack: HistoryStack) {
     this.grimpan = grimpan;
+    this.stack = new HistoryStack();
   }
 
   abstract initialize (): void;
 
-  static getInstance(grimpan: Grimpan) {}
+  getStack() {
+    return this.stack.clone();
+  }
+
+  setStack(stack: HistoryStack) {
+    this.stack = stack.clone();
+  }
+
+  static getInstance(grimpan: Grimpan, stack: HistoryStack) {}
 }
 
 export class IEGrimpanHistory extends GrimpanHistory {
@@ -20,7 +41,7 @@ export class IEGrimpanHistory extends GrimpanHistory {
 
   static override getInstance(grimpan: Grimpan) {
     if(!this.instance) {
-      this.instance = new IEGrimpanHistory(grimpan);
+      this.instance = new IEGrimpanHistory(grimpan, new HistoryStack());
     }
 
     return this.instance;
@@ -34,8 +55,8 @@ export class ChromeGrimpanHistory extends GrimpanHistory {
   }
   static override getInstance(grimpan: ChromeGrimpan): ChromeGrimpanHistory {
     if (!this.instance) {
-      this.instance = new ChromeGrimpanHistory(grimpan)
+      this.instance = new ChromeGrimpanHistory(grimpan, new HistoryStack());
     }
     return this.instance;
   }
-}
+} 
